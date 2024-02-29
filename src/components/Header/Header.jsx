fd6a40a44317b43f5481e94b6e16e5b5d1d9b911
RenatofilhoDevandtech <renatogt8@gmai.com>
2024-02-29 02:15:23 -0300

    melhorias e troca de imagens

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,27 @@
-/** @format */
-
-import "./Header.css";
+import React from 'react';
+import './Header.css';
 
 function Header() {
   return (
-    <>
-      <header>
+    <header className="prime-header">
+      <div className="logo">
+        {/* Adicione sua logo aqui */}
+        <img src="./assets/Logo.png" alt="Logo da Minha Empresa" className="logo-image" />
+      </div>
+      <nav className="nav-links">
         <ul>
-          <li>Inicio</li>
-          <li>Filmes</li>
-          <li>Series</li>
+          <li>
+            <a href="/" className="nav-link">Início</a>
+          </li>
+          <li>
+            <a href="/filmes" className="nav-link">Filmes</a>
+          </li>
+          <li>
+            <a href="/series" className="nav-link">Séries</a>
+          </li>
         </ul>
-      </header>
-    </>
+      </nav>
+    </header>
   );
 }
 
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,15 +1,19 @@
-/** @format */
-
 import React, { useState } from "react";
 import "./Main.css";
-import cris from "../../assets/cris.png";
+import filme2 from "../../assets/filme2.jpg";
+import filme5 from "../../assets/filme5.png";
+import filme4 from "../../assets/filme4.jpg";
+import filme6 from "../../assets/filme6.jpg";
+import filme3 from "../../assets/filme3.jpg";
+import filme1 from "../../assets/filme1.jpg";
 
 export default function Main() {
-  //criando um state
-  const [filme, setFilme] = useState();
+  // Criando um estado para armazenar a descrição do filme
+  const [filme, setFilme] = useState("");
 
-  function descricao() {
-    setFilme("Todo mundo odeia o chris, lançado em 2001, ator x");
+  // Função para atualizar a descrição do filme quando uma imagem é clicada
+  function descricao(descricaoFilme) {
+    setFilme(descricaoFilme);
   }
 
   return (
@@ -18,17 +22,59 @@ export default function Main() {
         <section className="banner"></section>
 
         <section className="cards">
+          {/* Primeira imagem */}
           <div>
-            <img onClick={descricao} src={cris} alt="logo" />
-            <p> {filme} </p>
+            <img
+              onClick={() => descricao("Todo mundo odeia o Chris, lançado em 2001, ator x")}
+              src={filme1}
+              alt="logo"
+            />
+            <p>{filme}</p>
           </div>
+          {/* Segunda imagem */}
           <div>
-            <img src={cris} alt="logo" />
-            <p></p>
+            <img
+              onClick={() => descricao("Descrição do filme 2")}
+              src={filme3}
+              alt="logo"
+            />
+            <p>{filme}</p>
           </div>
+          {/* Terceira imagem */}
           <div>
-            <img src={cris} alt="logo" />
-            <p></p>
+            <img
+              onClick={() => descricao("Descrição do filme 3")}
+              src={filme2}
+              alt="logo"
+            />
+            <p>{filme}</p>
+          </div>
+          {/* Quarta imagem */}
+          <div>
+            <img
+              onClick={() => descricao("Descrição do filme 4")}
+              src={filme5}
+              alt="logo"
+            />
+            <p>{filme}</p>
+          </div>
+          {/* Quinta imagem */}
+          <div>
+            <img
+              onClick={() => descricao("Descrição do filme 5")}
+              src={filme4}
+              alt="logo"
+            />
+            <p>{filme}</p>
+          </div>
+          {/* Sexta imagem */}
+          <div>
+            <img
+              onClick={() => descricao("Descrição do filme 6")}
+              src={filme6}
+              alt="logo"
+            />
+            <p>{filme}</p>
           </div>
         </section>
       </main>
